1617fc3d4bb74ca0bcbea79e8af18134b57d452e
Johan Sahlberg <44665925+JSahlberg@users.noreply.github.com>
2025-11-18 10:45:02 +0100

    updated version number

--- Koha/Plugin/Com/BM/BM_librisill.pm
+++ Koha/Plugin/Com/BM/BM_librisill.pm
@@ -48,7 +48,7 @@ use warnings;
 
 
 ## Here we set our plugin version
-our $VERSION = "0.5.4";
+our $VERSION = "0.5.5";
 our $MINIMUM_VERSION = "24.11";
 
 ## Here is our metadata, some keys are required, some are optional
@@ -56,7 +56,7 @@ our $metadata = {
     name            => 'BM Libris ILL module',
     author          => 'Johan Sahlberg',
     date_authored   => '2025-09-23',
-    date_updated    => "2025-11-17",
+    date_updated    => "2025-11-18",
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,
