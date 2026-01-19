051f5c5b6b754868af4d809cbceee9e7d98a9b23
Johan Sahlberg <44665925+JSahlberg@users.noreply.github.com>
2026-01-19 16:57:55 +0100

    updated version number

--- Koha/Plugin/Com/BM/BM_librisill.pm
+++ Koha/Plugin/Com/BM/BM_librisill.pm
@@ -48,7 +48,7 @@ use warnings;
 
 
 ## Here we set our plugin version
-our $VERSION = "0.7.4";
+our $VERSION = "0.7.5";
 our $MINIMUM_VERSION = "24.11";
 
 ## Here is our metadata, some keys are required, some are optional
@@ -1855,5 +1855,6 @@ sub _append_to_field {
 }
 
 1;
+
 
 
